0ff52178a3e2e199260fcb12ca2d7175af6d64aa
2110905h <2110905h@stu.kobe-u.ac.jp>
2022-10-14 15:01:47 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,15 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  createCanvas(300,300);
+  background(300);
+  noStroke();
+  for(let i = 11; i > 0 ; i--){
+    ellipse(100,100,i * 10 );
+    if(i<7){
+      stroke(0 ,0 , 255);
   }
+    else{
+      stroke(255, 0, 0);
+  }
+}
 }
